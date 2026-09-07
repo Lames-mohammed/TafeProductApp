ed2854f5ea631a6c69b10e7556d25092b6b93c1b
Lames Mohammed <lames.mohammed@student.tafesa.edu.au>
2026-09-07 13:34:25 +0930

    Commit the branch feat_GST1.2

--- ProductMaintenance/ProductApps/MainWindow.xaml.cs
+++ ProductMaintenance/ProductApps/MainWindow.xaml.cs
@@ -36,6 +36,8 @@ namespace ProductApps
                 totalPaymentTextBlock.Text = Convert.ToString(cProduct.TotalPayment);
                 totalChargeTextBox.Text = Convert.ToString(cProduct.TotalPayment + 25);
                 totalChargeafterWrapTextBox.Text = Convert.ToString(cProduct.TotalPayment + 25 + 5);
+                TotalChargeAfterGSTextBox.Text = Convert.ToString((cProduct.TotalPayment + 25 + 5) * 1.1m); 
+
             }
             catch (FormatException)
             {
